6cf56e879dd315e9a717ed0bd2af40dc7cde653a
Ana Machado <144913485+a2023132635@users.noreply.github.com>
2026-01-28 15:54:03 +0000

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,19 @@
+<?php
+define('BASE_DIR', dirname(__DIR__) . '/');
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "banco_novo";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+} catch(PDOException $e) {
+    die("Erro na ligação: " . $e->getMessage());
+}
+?>
